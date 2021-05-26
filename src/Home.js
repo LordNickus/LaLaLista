f8050d9493fe7731f67450509092afcab070858d
LordNickus <79268371+LordNickus@users.noreply.github.com>
2021-05-26 20:22:20 -0300

    arreglito

--- src/Home.js
+++ src/Home.js
@@ -29,9 +29,9 @@ const Home = ()=> {
                  </Button>
                  </div>
             </div>
-            <div>
+            {/* <div>
                 
-            </div>
+            </div> */}
         </div>
     )
 }
